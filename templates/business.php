b387c32e03a7c836c10383fb7ab11c4101d393e3
Avallon Azevedo <hello@avallonazevedo.dev>
2022-01-10 21:25:46 -0300

    feat(business): creating benefits section

--- templates/business.php
+++ templates/business.php
@@ -25,4 +25,23 @@
 </section>
 <!-- /#intro.business -->
 
+<section id="benefits">
+    <div class="container small">
+        <div class="content">
+            <div class="entry-text text blue">
+                Onde tem Grupo Malwee, tem prosperidade e a melhor experiência de marca para os consumidores.
+            </div>
+            <!-- /.entry-text text blue -->
+
+            <div class="description">
+                <p>Estamos presentes em mais de 25 mil pontos de venda em uma completa rede de multimarcas, franquias, lojas fidelizadas, próprias e canais online. Nossos lojistas contam com estrutura especializada de atendimento e todas as ferramentas digitais e programas de treinamento exclusivos para potencializar os negócios e alcançar os melhores resultados. </p>
+            </div>
+            <!-- /.description -->
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.container medium -->
+</section>
+<!-- /#benefits -->
+
 <?php get_footer(); ?>
