448ea3dc7322a7380dbf6987c0a044aaf04c4075
Avallon Azevedo <hello@avallonazevedo.dev>
2022-01-10 21:12:32 -0300

    feat(business): creating page intro section

--- templates/business.php
+++ templates/business.php
@@ -5,4 +5,24 @@
 	get_header();
 ?>
 
+<section id="intro" class="business">
+	<div class="content">
+        <div class="intro-text">
+            <strong>Malwee,</strong>
+            <span>Presente em todo o Brasil e no coração das pessoas. </span>
+        </div>
+        <!-- /.intro-text -->
+
+        <div class="cover-image">
+            <img src="<?php echo THEME_IMAGES . '/intro/business.jpg' ?>" alt="Negócio Malwee">
+        </div>
+        <!-- /.cover-image -->
+	</div>
+	<!-- /.content -->
+
+	<div class="circle right green"></div>
+
+</section>
+<!-- /#intro.business -->
+
 <?php get_footer(); ?>
