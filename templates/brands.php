c0972947e6ba9891836b7d606d128dd7873d1e8b
Avallon Azevedo <hello@avallonazevedo.dev>
2022-01-12 20:59:18 -0300

    feat(brands): creating brand disclaimer section

--- templates/brands.php
+++ templates/brands.php
@@ -6,4 +6,20 @@
 	get_header();
 ?>
 
+
+<section id="brand-intro" class="cover-image bg-image">
+    <img src="<?php echo THEME_IMAGES . '/brands/malwee-intro.jpg' ?>" alt="Malwee">
+    <h1 class="text text-white"><?php the_title(); ?></h1>
+</section>
+<!-- /#brand-intro -->
+
+<section id="brand-disclaimer" class="bg-light-grey">
+    <div class="container small text text-dark-blue">
+        <h2>Moda sem ponto final.</h2>
+        <p>A Malwee faz moda sem ponto final. Roupas que duram e não se perdem a cada coleção. Feitas de um jeito mais sustentável. Atemporal e democrática, para diferentes biotipos, idades e estilos.</p>
+    </div>
+    <!-- /.container small -->
+</section>
+<!-- /#brand-disclaimer -->
+
 <?php get_footer(); ?>
